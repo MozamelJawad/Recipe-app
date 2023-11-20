248fdbb2fd60c071d8eb90824e9d6da8dad6c13d
Agustin Soria <soriaagustin8@gmail.com>
2023-11-20 10:10:49 -0300

    Update Recipe Food table

--- db/migrate/20231120130514_create_recipe_food.rb
+++ db/migrate/20231120130514_create_recipe_food.rb
@@ -1,7 +1,9 @@
 class CreateRecipeFood < ActiveRecord::Migration[7.1]
   def change
     create_table :recipe_foods do |t|
-
+      t.integer :quantity
+      t.reference :recipes, null: false, foreign_key: true
+      t.reference :foods, null: false, foreign_key: true
       t.timestamps
     end
   end
