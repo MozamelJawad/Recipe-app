75e252d031bf2fbca614ff789a60a5e01b9cfaeb
Agustin Soria <soriaagustin8@gmail.com>
2023-11-20 10:04:53 -0300

    Update version of Create recipe table

--- db/migrate/20231120124754_create_recipe.rb
+++ db/migrate/20231120124754_create_recipe.rb
@@ -1,7 +1,12 @@
 class CreateRecipe < ActiveRecord::Migration[7.1]
   def change
     create_table :recipes do |t|
-
+      t.reference :user, null: false, foreign_key: true
+      t.string :name
+      t.time :preparation_time
+      t.time :cooking_time
+      t.text :description
+      t.bool :public
       t.timestamps
     end
   end
