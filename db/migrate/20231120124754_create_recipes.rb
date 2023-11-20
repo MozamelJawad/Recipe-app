f13ff6c8820fcd05700cc0d9f937ea695762dcac
Mozamel Jawad <mozameljawad@gmail.com>
2023-11-20 18:11:58 +0430

    Add remaining migration

--- db/migrate/20231120124754_create_recipes.rb
+++ db/migrate/20231120124754_create_recipes.rb
@@ -1,12 +1,13 @@
-class CreateRecipe < ActiveRecord::Migration[7.1]
+class CreateRecipes < ActiveRecord::Migration[7.1]
   def change
     create_table :recipes do |t|
-      t.reference :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: { on_delete: :cascade, on_update: :cascade}
       t.string :name
       t.decimal :preparation_time
       t.decimal :cooking_time
       t.text :description
       t.boolean :public
+
       t.timestamps
     end
   end
