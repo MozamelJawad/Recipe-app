57bdd8eafca3ea5650e7525d6dabf68017137f84
Agustin Soria <soriaagustin8@gmail.com>
2023-11-22 01:11:23 -0300

    Adding recipes controller

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,29 @@
+class RecipesController < ApplicationController
+    def index
+      @recipes = Recipe.all
+    end
+
+    def create
+        @recipes = current_user.recipes.build(recipe_params)
+    
+        if @recipes.save
+          redirect_to recipes_path, notice: 'Food was successfully created.'
+        else
+          render :new
+        end
+    end
+
+    def destroy
+      @recipe = Recipe.find(params[:id])
+      @recipe.destroy
+
+      redirect_to recipes_path, notice: "Recipe eliminated successfully."
+    end
+    def show
+        @recipes = Recipes.find(params[:id])
+    end
+    
+    def recipe_params
+        params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+    end
+end
