e74d3cd67dc592c913014bd2bd1b3b061ddcb44c
Mozamel Jawad <mozameljawad@gmail.com>
2023-11-24 22:07:23 +0430

    add test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,5 +11,9 @@ RSpec.describe User, type: :model do
     it 'should have a email address' do
       expect(user.email).to eq('ahmad@gmail.com')
     end
+
+    it 'is not valid without email' do
+      expect(User.create(name: 'Tom')).to_not be_valid
+    end
   end
 end
