d50b8e78a21a2c335d747ef2e1d650e26b18be23
Mozamel Jawad <mozameljawad@gmail.com>
2023-11-24 15:10:32 +0430

    complete shopping list controller

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,15 +1,27 @@
 class ShoppingListsController < ApplicationController
-    include ShoppingListsHelper
-  
-    def index
-      @recipes = Recipe.find(params[:recipe_id])
-      @total_foods_value = calc_food
+  def index
+    # Get all the foods of the current user
+    @foods = current_user.foods
+
+    # For each recipe of the current user, update the quantity of the foods
+    current_user.recipes.map do |recipe|
+      recipe.recipe_foods.includes(:food).map do |recipe_food|
+        food = recipe_food.food
+        selected_food = @foods.select { |f| f.name == food.name }[0]
+        selected_food.quantity = selected_food.quantity - recipe_food.quantity
+      end
     end
 
-    private
+    # Select only the foods with negative quantity
+    @foods = @foods.select { |food| food.quantity.negative? }
 
-    def calc_food
-      @food = Food.find(params[:name])
-      @
+    # Invert the quantity of the selected foods
+    @foods.each { |food| food.quantity *= -1 }
+
+    # Calculate the total price of the selected foods
+    @total = 0
+    @foods.each do |food|
+      @total += (food.price * food.quantity)
     end
   end
+end
